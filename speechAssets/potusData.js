70160024ec566184e4fef0699e5efc4bab879628
Russell Schmidt <russell.schmidt@me.com>
2016-04-04 01:04:06 -0700

    first commit

---
+++ speechAssets/potusData.js
@@ -0,0 +1,56 @@
+func Potus (fname, mname, lname, nickname) {
+  self.firstName = fname;
+  self.middleName = mname;
+  self.lastName = lname;
+  self.fullName = firstName + " " + middleName + " " + lastName;
+  self.nickname = nickname;
+
+}
+
+var potusArray = [];
+
+potusArray.push(new President("George","","Washington"));
+potusArray.push(new President("John","","Adams"));
+potusArray.push(new President("Thomas","","Jefferson"));
+potusArray.push(new President("James","","Madison"));
+potusArray.push(new President("James","","Monroe"));
+potusArray.push(new President("John","Quincy","Adams"));
+potusArray.push(new President("Andrew","","Jackson"));
+potusArray.push(new President("Martin","","Van Buren"));
+potusArray.push(new President("William","Henry","Harrison"));
+potusArray.push(new President("John","","Tyler"));
+potusArray.push(new President("James","Knox","Polk"));
+potusArray.push(new President("Zachary","","Taylor"));
+potusArray.push(new President("Millard","","Fillmore"));
+potusArray.push(new President("Franklin","","Pierce"));
+potusArray.push(new President("James","","Buchanan"));
+potusArray.push(new President("Abraham","","Lincoln"));
+potusArray.push(new President("Andrew","","Johnson"));
+potusArray.push(new President("Ulysses","S.","Grant"));
+potusArray.push(new President("Rutherford","Birchard","Hayes"));
+potusArray.push(new President("James","Abram","Garfield"));
+potusArray.push(new President("Chester","Alan","Arthur"));
+potusArray.push(new President("Stephen","Grover","Cleveland"));
+potusArray.push(new President("Benjamin","","Harrison"));
+potusArray.push(new President("Stephen","Grover","Cleveland"));
+potusArray.push(new President("William","","McKinley"));
+potusArray.push(new President("Theodore","","Roosevelt"));
+potusArray.push(new President("William","Howard","Taft"));
+potusArray.push(new President("Thomas","Woodrow","Wilson"));
+potusArray.push(new President("Warren","Gamaliel","Harding"));
+potusArray.push(new President("John","Calvin","Coolidge"));
+potusArray.push(new President("Herbert","Clark","Hoover"));
+potusArray.push(new President("Franklin","Delano","Roosevelt"));
+potusArray.push(new President("Harry","S.","Truman"));
+potusArray.push(new President("Dwight","David","Eisenhower"));
+potusArray.push(new President("John","Fitzgerald","Kennedy"));
+potusArray.push(new President("Lyndon","Baines","Johnson"));
+potusArray.push(new President("Richard","Milhous","Nixon"));
+potusArray.push(new President("Gerald","Rudolph","Ford"));
+potusArray.push(new President("James","Earl","Carter"));
+potusArray.push(new President("Ronald","Wilson","Reagan"));
+potusArray.push(new President("George","Herbert Walker","Bush"));
+potusArray.push(new President("William","Jefferson","Clinton"));
+potusArray.push(new President("George","Walker","Bush"));
+potusArray.push(new President("Barack","Hussein","Obama"));
+
